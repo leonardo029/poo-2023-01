663f65cb1ca064a3e1b59cb0af838756290838dd
Leonardo Moreira <moreira.leonardo83@oulook.com>
2023-07-08 18:27:11 -0300

    Mudanças

--- t13/src/main/java/com/github/leonardo029/poo/t13/MyClass.java
+++ t13/src/main/java/com/github/leonardo029/poo/t13/MyClass.java
@@ -1,6 +1,6 @@
 package com.github.leonardo029.poo.t13;
 
-public class Main {
+public class MyClass {
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
