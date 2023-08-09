905803f13abfa3bf1539ea8433e43c96f4dfc1e9
Leonardo Moreira <moreira.leonardo83@oulook.com>
2023-08-09 15:43:45 -0300

    Finalização da atividade

---
+++ t21/src/main/java/com/github/leonardo029/poo/t21/GerenteRestaurante.java
@@ -0,0 +1,9 @@
+package com.github.leonardo029.poo.t21;
+
+import java.util.List;
+
+public class GerenteRestaurante implements Trabalhador {
+    
+    private String nome;
+    private List<Ordem> ordensDadas;
+}
